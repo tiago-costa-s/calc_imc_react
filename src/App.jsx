9cbcdd4dc8b1a5a2a455788375fdcd5bc19b69dd
tGUhh <tiagocostasantos0013@gmail.com>
2023-11-30 09:22:25 -0300

    Adicionado Handle function / Estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
 
     <div className="app">
--- src/assets/components/formButton/FormButton.jsx
+++ src/assets/components/formButton/FormButton.jsx
@@ -3,11 +3,9 @@ import "./FormButton.css";
 const FormButton = ({ variant, action, text }) => {
 
     return (
-
         <button className={variant} onClick={action}>{text}</button>
+    );
 
-    )
+};
 
-}
-
-export default FormButton
+export default FormButton;
