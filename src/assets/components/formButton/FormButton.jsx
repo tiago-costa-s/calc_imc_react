b880d206c3239a8423fb3cffd7e99286cd39a7aa
tGUhh <tiagocostasantos0013@gmail.com>
2023-11-30 08:36:03 -0300

    Inicializado FormButton

---
+++ src/assets/components/formButton/FormButton.jsx
@@ -0,0 +1,13 @@
+import "./FormButton.css";
+
+const FormButton = ({ variant, action, text }) => {
+
+    return (
+
+        <button className={variant} onClick={action}>{text}</button>
+
+    )
+
+}
+
+export default FormButton
