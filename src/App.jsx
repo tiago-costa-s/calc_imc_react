9a6f21ad78ba18741857555bbfcf33153c301b8c
tGUhh <tiagocostasantos0013@gmail.com>
2023-12-04 08:35:21 -0300

    Adicionado inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import ControlInput from './components/controlInput/ControlInput'
 
 import './App.css'
+import CalculatorImc from './components/calculatorImc/CalculatorImc'
 
 function App() {
 
   return (
 
     <div className="app">
-      
+      <CalculatorImc />
     </div>
 
   )
