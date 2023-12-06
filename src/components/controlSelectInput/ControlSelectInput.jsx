d601f0cb5b06d4f96b15e678f2739383ce773865
tGUhh <tiagocostasantos0013@gmail.com>
2023-12-06 18:34:59 -0300

    Adicionar função handleSelectChange

--- src/components/controlSelectInput/ControlSelectInput.jsx
+++ src/components/controlSelectInput/ControlSelectInput.jsx
@@ -1,18 +1,29 @@
+// Hooks
+import { useState } from 'react';
+// CSS
 import './ControlSelectInput.css';
 
-const ControlSelectInput = ({ position, label, variant, name, id, option }) => {
+const ControlSelectInput = ({ position, label, variant, name, id }) => {
+
+    const [select, setSelect] = useState();
+
+    const handleSelectChange = (e) => {
+        const selectValue = e.target.value;
+        setSelect(selectValue);
+        console.log(typeof selectValue);
+    };
 
     return (
 
-        <div className='container-select'>
+        <div className={`container-select ${position}`} >
             <label htmlFor="">{label}</label>
 
-            <select className={variant} name={name}>
+            <select className={variant} name={name} value={select} onChange={handleSelectChange}>
                 <option value=''>- -</option>
                 <option value='man'>Masculino</option>
                 <option value='woman'>Feminino</option>
             </select>
-        </div>
+        </div >
 
     )
 
