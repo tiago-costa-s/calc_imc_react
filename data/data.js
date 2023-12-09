3ef2b756074c9616b9c7b4ef9010739ee3f6a494
tGUhh <tiagocostasantos0013@gmail.com>
2023-12-09 14:53:52 -0300

    Adicionado dados da tabela de IMC

--- data/data.js
+++ data/data.js
@@ -1,7 +1,44 @@
 export const data = [
     {
-        min: 11,
-        max: 18,
-        title: 'Normal'
-    }
+        min: 14,
+        max: 19,
+        info: 'Abaixo do peso',
+        description: 'Entre 14 e 19',
+    },
+
+    {
+        min: 19,
+        max: 23.9,
+        info: 'Normal',
+        description: 'Entre 19 e 23.9',
+    },
+
+    {
+        min: 24,
+        max: 28.9,
+        info: 'Sobre Peso',
+        description: 'Entre 24 e 28.9',
+    },
+
+    {
+        min: 29,
+        max: 39.9,
+        info: 'Obesidade I',
+        description: 'Entre 29 e 39.9',
+    },
+
+    {
+        min: 29,
+        max: 39.9,
+        info: 'Obesidade II',
+        description: 'Entre 29 e 39.9',
+    },
+
+    {
+        min: 40,
+        max: 99.9,
+        info: 'Obesidade III',
+        description: 'Maior ou igual a 40',
+    },
+   
 ];
