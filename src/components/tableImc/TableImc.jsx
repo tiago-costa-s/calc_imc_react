23c501029327cf715e7cc7bb4928a7134bed5f9e
tGUhh <tiagocostasantos0013@gmail.com>
2023-12-12 10:17:15 -0300

    Remoção do arquivo teste.js

--- src/components/tableImc/TableImc.jsx
+++ src/components/tableImc/TableImc.jsx
@@ -6,26 +6,18 @@ import './TableImc.css';
 
 const TableImc = ({ data, imc, info }) => {
 
-    let alertColor;
-
-    data.map((tab) => {
-        if (imc >= tab.min && imc <= tab.max) {
-            alertColor = tab.alert;
-        }
-    });
-
     return (
 
         <div className='table-imc'>
             <div className='result-header'>
-                <p>Seu IMC <span className={alertColor} id='imc'>{imc}</span></p>
-                <p>Sua Classificação <span className={alertColor} id='info'>{info}</span></p>
+                <p>Seu IMC <span className='' id='imc'>{imc}</span></p>
+                <p>Sua Classificação <span id='info'>{info}</span></p>
             </div>
             <div className='table-header'>
                 <p>Classificações</p>
                 <p>Informação</p>
             </div>
-            <div className='table-body'>
+            <div className="table-body">
                 {data.map((table) => (
                     <div className='table-column' key={table.description}>
                         <p className='line-description'>{table.description}</p>
